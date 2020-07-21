ad682e30028201f8d0f8d02d16698deb25f9d589
Moon04 <30897431+Moon04@users.noreply.github.com>
2020-07-21 15:08:12 +0200

    update items

--- app/service.py
+++ app/service.py
@@ -1,15 +1,47 @@
 from .models.item import Item
 
-items=[
-  Item(name='Aged Brie',sell_in=1,quality=1),
-  Item(name='Backstage passes to a TAFKAL80ETC concert',sell_in=2,quality=2),
-  Item(name='Sulfuras, Hand of Ragnaros',sell_in=3,quality=3),
-  Item(name='Conjured',sell_in=4,quality=4)
+items = [
+    Item(name='Aged Brie', sell_in=1, quality=1),
+    Item(name='Backstage passes to a TAFKAL80ETC concert', sell_in=2, quality=2),
+    Item(name='Sulfuras, Hand of Ragnaros', sell_in=3, quality=3),
+    Item(name='Conjured', sell_in=4, quality=4)
 ]
 
 
 def getItems():
-  return items
+    return items
 
-def updateItem():
-  return ''
+
+def createItem(item):
+    items.extend(item)
+
+
+def updateItems():
+    for item in items:
+        if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
+            if item.quality > 0:
+                if item.name != "Sulfuras, Hand of Ragnaros":
+                    item.quality = item.quality - 1
+        else:
+            if item.quality < 50:
+                item.quality = item.quality + 1
+                if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                    if item.sell_in < 11:
+                        if item.quality < 50:
+                            item.quality = item.quality + 1
+                    if item.sell_in < 6:
+                        if item.quality < 50:
+                            item.quality = item.quality + 1
+        if item.name != "Sulfuras, Hand of Ragnaros":
+            item.sell_in = item.sell_in - 1
+        if item.sell_in < 0:
+            if item.name != "Aged Brie":
+                if item.name != "Backstage passes to a TAFKAL80ETC concert":
+                    if item.quality > 0:
+                        if item.name != "Sulfuras, Hand of Ragnaros":
+                            item.quality = item.quality - 1
+                else:
+                    item.quality = item.quality - item.quality
+            else:
+                if item.quality < 50:
+                    item.quality = item.quality + 1
--- app/views/home/views.py
+++ app/views/home/views.py
@@ -1,25 +1,28 @@
-from flask import redirect,render_template,url_for
+from flask import redirect, render_template, url_for
 from .forms import CreateItem
 from . import home
 from ...models.item import Item
 
-# @home.route('/')
-# def home():
-#     return render_template('home/index.html',len = len(items),items=items)
+from ...service import createItem, updateItems, getItems
 
-@home.route('/create',methods=['GET','POST'])
+
+@home.route('/create', methods=['GET', 'POST'])
 def create():
+    form = CreateItem()
+    if form.validate_on_submit():
+        item = Item(name=form.name.data, sell_in=form.sell_in.data,
+                    quality=form.quality.data)
+        # Add item to array
+        createItem(item)
+        # go to list view
+        return render_template(url_for('createItem.html'))
 
-  form=CreateItem()
-  if form.validate_on_submit():
-    item=Item(name=form.name.data,sell_in=form.sell_in.data,quality=form.quality.data)
-    # Add item to array
-    items.extend(item)
+    # load create item view
+    return render_template('home/createItem.html', form=form, title="Create Item")
 
-    # go to list view 
-    return render_template(url_for('createItem.html'))
 
-  # load create item view
-  return render_template('home/createItem.html',form=form,title="CreateItem")
-
-  
+@home.route('/update', methods=['GET'])
+def update():
+    updateItems()
+    # load create item view
+    return render_template('home/index.html', len=len(getItems()), items=getItems())
